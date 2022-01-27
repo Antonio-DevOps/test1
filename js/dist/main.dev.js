49610631e72b65ecd8fd45b29db3f19937105a44
Ksion4ik <ksion4ik.life@gmail.com>
2022-01-27 13:31:10 +0200

    Add new files

---
+++ js/dist/main.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log('arr' + ' - object');
+console.log(4 + ' - object');
+var incr = 10,
+    decr = 10; // incr++;
+// decr--;
+// ++incr;
+// --decr;
+
+console.log(incr);
+console.log(decr);
